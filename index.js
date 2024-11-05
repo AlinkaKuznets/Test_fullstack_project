1bd8803669c8d49716d4808b1c46ee69b6464f53
AlinkaKuznets <linaolen6@mail.ru>
2024-11-06 01:27:18 +0700

    Added function Login and middleware checkAuth.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import mongoose from "mongoose";
 import { validationResult } from "express-validator"
 
 import { registerValidator } from "./validations/auth.js"
+
 import UserModel from './models/user.js'
+import checkAuth from './utils/checkAuth.js'
 
 mongoose.connect('mongodb+srv://admin:180801Ak@cluster0.wrfph.mongodb.net/blog')
     .then(() => console.log('DB ok'))
@@ -14,7 +16,47 @@ mongoose.connect('mongodb+srv://admin:180801Ak@cluster0.wrfph.mongodb.net/blog')
 const app = express();
 app.use(express.json());
 
-app
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+
+        if (!user) {
+            return req.status(400).json({
+                massege: 'Ползователь не найден',
+            });
+        };
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if (!isValidPass) {
+            return res.status(400).json({
+                massege: 'Неверный логин или пароль',
+            });
+        }
+
+        const token = jwt.sign({
+            _id: user._id,
+
+        }, 'secret123', {
+            expiresIn: '30d'
+        });
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json(
+            {
+                ...userData,
+                token,
+            }
+        );
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: ' Не удалось выполнить подключение'
+        });
+    }
+});
 
 app.post('/auth/register', registerValidator, async (req, res) => {
     try {
@@ -41,7 +83,7 @@ app.post('/auth/register', registerValidator, async (req, res) => {
 
         }, 'secret123', {
             expiresIn: '30d'
-        })
+        });
 
         const { passwordHash, ...userData } = user._doc;
 
@@ -56,6 +98,24 @@ app.post('/auth/register', registerValidator, async (req, res) => {
         res.status(500).json({
             message: ' Не удалось выполнить подключение'
         });
+    }
+});
+
+app.get('/auth/me', checkAuth, async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId);
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Полбзователь не найден',
+            });
+        }
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json(userData);
+
+    } catch (err) {
+
     }
 });
 
